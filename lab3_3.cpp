d38d868709727a645ed956141fd765cba106dca3
unknown <rungrodks@hotmail.com>
2019-01-14 15:31:31 +0700

    exercise3_3

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int num = 10;
+    int num = 3;
     int count = num;
     int k;
     int ans = 0;
-    while(count <= 50){
+    while(count <= 300){
         k = count*count;
         ans = ans + k;
-        count = count + 1;
+        count = count + 3;
     }
     cout << ans;
     
